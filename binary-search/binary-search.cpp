047e8f2ae71694b0c36a7b4d0c56d44d8f9c0aaf
RohithaSGobbani <rohitshreyas887@gmail.com>
2026-02-21 22:27:34 +0530

    Fix binary search logic for correct comparisons

--- binary-search/binary-search.cpp
+++ binary-search/binary-search.cpp
@@ -8,7 +8,7 @@ int bs(vector<int> &arr, int t){
     while(low <= high){
         int mid = (low + high) / 2;
         if(arr[mid] == t) return mid;
-        else if(arr[mid] > t) low = mid+1;
+        else if(arr[mid] < t) low = mid+1;
         else high = mid - 1;
     }
     return -1;
@@ -19,7 +19,7 @@ int bsRecursive(vector<int> &arr, int low, int high, int t){
     if(low > high) return -1;
     int mid = (low+high)/2;
     if(arr[mid] == t) return mid;
-    else if(arr[mid] > t)
+    else if(arr[mid] < t)
         return bsRecursive(arr,mid+1, high , t);
     else
         return bsRecursive(arr, low, mid-1, t);
