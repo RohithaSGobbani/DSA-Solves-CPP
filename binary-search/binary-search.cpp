5be7c988070cf9596d1ffe4b49a675d335efaf30
RohithaSGobbani <rohitshreyas887@gmail.com>
2026-02-21 22:07:47 +0530

    feat: add binary search algorithm(Recursive)

--- binary-search/binary-search.cpp
+++ binary-search/binary-search.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//Iterative
 int bs(vector<int> &arr, int t){
     int n = arr.size();
     int low = 0, high = n-1;
@@ -13,6 +14,17 @@ int bs(vector<int> &arr, int t){
     return -1;
 }
 
+//Recursive
+int bsRecursive(vector<int> &arr, int low, int high, int t){
+    if(low > high) return -1;
+    int mid = (low+high)/2;
+    if(arr[mid] == t) return mid;
+    else if(arr[mid] > t)
+        return bsRecursive(arr,mid+1, high , t);
+    else
+        return bsRecursive(arr, low, mid-1, t);
+}
+
 int main(){
     int n;
     cin >> n;
@@ -20,6 +32,6 @@ int main(){
     for(int i = 0; i < n; i++){
         cin >> arr[i];
     }
-    cout << bs(arr, 6);
+    cout << bsRecursive(arr,0,n-1, 6);
     return 0;
 }
