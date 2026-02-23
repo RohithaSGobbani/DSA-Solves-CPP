9d169cf3bd102f08036da18c06f6a4f9a8834b56
RohithaSGobbani <rohitshreyas887@gmail.com>
2026-02-23 22:22:50 +0530

    Implement first and last occurrence functions using normal binary search
    Added functions to find the first and last occurrences of a target in a sorted array using binary search.

--- binary-search/first-and-last-occurnces.cpp
+++ binary-search/first-and-last-occurnces.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//using lowerbound and upperbound
 int lowerBound(vector<int> &arr, int target){
     int n = arr.size();
     int low = 0, high = n-1;
@@ -44,6 +45,43 @@ vector<int> searchRange(vector<int>& arr, int target) {
 Input: nums = [5,7,7,8,8,10], target = 8
 Output: [3,4]
 */
+
+//Using normal binary search
+int firstOccurence(vector<int> &arr, int target){
+    int n = arr.size();
+    int low = 0, high = n-1;
+    int first = -1;
+    while(low <= high){
+        int mid = low + (high - low)/2;
+        if(arr[mid] == target){
+            first = mid;
+            high = mid-1;
+        }else if(arr[mid] < target){
+            low = mid+1;
+        }else{
+            high = mid -1;
+        }
+    }
+    return first;
+}
+
+int lastOccurence(vector<int> &arr, int target){
+    int n = arr.size();
+    int low = 0, high = n-1;
+    int last = -1;
+    while(low <= high){
+        int mid = low + (high - low)/2;
+        if(arr[mid] == target){
+            last = mid;
+            low = mid + 1;
+        }else if(arr[mid] < target){
+            low = mid+1;
+        }else{
+            high = mid -1;
+        }
+    }
+    return last;
+}
 
 int main(){
     int n;
