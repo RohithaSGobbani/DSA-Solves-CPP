0858ca585c081189c82894f07e6a36f3c2f016a2
RohithaSGobbani <rohitshreyas887@gmail.com>
2026-03-10 21:21:25 +0530

    feat: add median-of-two-sorted-arrays.cpp optimal solution

--- binary-search/median-of-two-sorted-arrays.cpp
+++ binary-search/median-of-two-sorted-arrays.cpp
@@ -56,6 +56,37 @@ int medianBetter(vector<int> &arr1, vector<int> &arr2){
     else return (ind2ele + ind1ele)/ 2.0;
 }
 
+//Optimal
+int medianOptimal(vector<int> &arr1, vector<int> &arr2){
+    int n1 = arr1.size();
+    int n2 = arr2.size();
+    if(n1 > n2) return medianOptimal(arr2, arr1);
+    int low = 0, high = n1;
+    int left = (n1+n2+1)/2;
+    int n = n1+n2;
+    while(low <= high){
+        int mid1 = low + (high - low) >> 1;
+        int mid2 = left - mid1;
+        int l1 = INT_MIN, l2 = INT_MIN;
+        int r1 = INT_MAX, r2 = INT_MAX;
+        if(mid1 < n1) r1 = arr1[mid1];
+        if(mid2 < n2) r2 = arr2[mid2];
+        if(mid1 - 1 >= 0) l1 = arr1[mid1-1];
+        if(mid2-1 >= 0) l2 = arr2[mid2-1];
+        if(l1 <= r2 && l2 <= r1){
+            if(n % 2 == 1) return max(l1,l2);
+            return (double)(max(l1,l2) + min(r1,r2)) / 2.0;
+        }else if(l1 > r2) high = mid1-1;
+        else low = mid1+1;
+    }
+    return 0;
+}
+
+/*
+Input: nums1 = [1,2], nums2 = [3,4]
+Output: 2.50000
+*/
+
 int main(){
     int n1, n2;
     cin >> n1 >> n2;
@@ -67,6 +98,6 @@ int main(){
     for(int i = 0; i < n2; i++){
         cin >> arr2[i];
     }
-    cout<< medianBetter(arr1, arr2);
+    cout<< medianOptimal(arr1, arr2);
     return 0;
 }
