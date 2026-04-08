47c3edaedfd12e64e9ba70ae1c842b6e73b80004
RohithaSGobbani <rohitshreyas887@gmail.com>
2026-04-08 21:20:51 +0530

    feat:add linked list length

--- linked-list/ll.cpp
+++ linked-list/ll.cpp
@@ -30,6 +30,16 @@ Node* convertArray2LL(vector<int> &arr){
     return head;
 }
 
+int lengthOfLL(Node* head){
+    int cnt = 0;
+    Node* temp = head;
+    while(temp){
+        temp = temp->next;
+        cnt++;
+    }    
+    return cnt;
+}
+
 int main(){
     vector<int> arr = {12,5,8,7};
     Node* head = convertArray2LL(arr);
@@ -38,4 +48,6 @@ int main(){
         cout << temp->data << " ";
         temp = temp->next;
     }
+    cout << endl;
+    cout << lengthOfLL(head);
 }
