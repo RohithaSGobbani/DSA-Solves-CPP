06636f7cde8dfb5e9b4806973bc071f41aeebce3
RohithaSGobbani <rohitshreyas887@gmail.com>
2026-04-08 21:18:20 +0530

    feat:add linked list traversal

--- linked-list/ll.cpp
+++ linked-list/ll.cpp
@@ -19,8 +19,23 @@ class Node{
     }
 };
 
+Node* convertArray2LL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i = 1; i < arr.size(); i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = mover->next;
+    }
+    return head;
+}
+
 int main(){
-    vector<int> arr = {2,5,0,7};
-    Node* y = new Node(arr[0]);
-    cout << y->data;
+    vector<int> arr = {12,5,8,7};
+    Node* head = convertArray2LL(arr);
+    Node* temp = head;
+    while(temp){
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
 }
