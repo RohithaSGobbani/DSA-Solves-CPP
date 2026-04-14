9670453e31ef28c0f33a2b55cac434ef3857a7ff
RohithaSGobbani <rohitshreyas887@gmail.com>
2026-04-14 15:37:50 +0530

    feat: add DLL-deleteHead

--- linked-list/dll.cpp
+++ linked-list/dll.cpp
@@ -43,8 +43,21 @@ void print(Node* head){
     cout << endl;    
 }
 
+Node* deleteHead(Node* head){
+    if(head == NULL || head->next == NULL) return NULL;
+    
+    Node* prev = head;
+    head = head->next;
+    head->back = nullptr;
+    prev->next = nullptr;
+
+    delete prev;
+    return head;
+}
+
 int main(){
     vector<int> arr = {10,5, 8, 9};
     Node* head = convertArray2DLL(arr);
+    head = deleteHead(head);
     print(head);
 }
