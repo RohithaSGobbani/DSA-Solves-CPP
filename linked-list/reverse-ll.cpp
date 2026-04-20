0631afa96199cb7accc4f9f3d5e3259dafd4ec50
RohithaSGobbani <rohitshreyas887@gmail.com>
2026-04-20 12:48:23 +0530

    feat: add reverse a ll brute solution

---
+++ linked-list/reverse-ll.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    public:
+    Node(int data1, Node* next1){
+        data = data1;
+        next = next1;
+    }
+
+    public:
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+
+Node* convertArray2LL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i = 1; i < arr.size(); i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = mover->next;
+    }
+    return head;
+}
+
+void print(Node* head){
+    Node* temp = head;
+    while(temp){
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;    
+}
+
+Node* reverseList(Node* head) {
+    if(head == NULL || head->next == NULL) return head;
+    stack<int> st;
+    Node* temp = head;
+    while(temp != NULL){
+        st.push(temp->data);
+        temp = temp->next;
+    }
+    temp = head;
+    while(temp != NULL){
+        temp->data = st.top();
+        st.pop();
+        temp = temp->next;
+    }
+    return head;
+}
+
+/*
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+*/
+
+int main(){
+    vector<int> arr = {1,2,3,4,5};
+    Node* head = convertArray2LL(arr);
+    print(head);
+    head = reverseList(head);
+    print(head);
+    return 0;
+}
