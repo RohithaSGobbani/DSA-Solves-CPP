e66c4f3197c8348825f90b7740571617d6c3b171
RohithaSGobbani <rohitshreyas887@gmail.com>
2026-05-12 19:54:53 +0530

    feat: add find-pairs-with-given-sum-in-dll optimal solution

--- linked-list/find-pairs-with-given-sum-in-dll.cpp
+++ linked-list/find-pairs-with-given-sum-in-dll.cpp
@@ -24,12 +24,12 @@ class Node{
 
 Node* convertArray2DLL(vector<int> &arr){
     Node* head = new Node(arr[0]);
-    Node* prev = head;
+    Node* back = head;
     int n = arr.size();
     for(int i = 1; i < n; i++){
-        Node* temp = new Node(arr[i], nullptr, prev);
-        prev->next = temp;
-        prev = temp;
+        Node* temp = new Node(arr[i], nullptr, back);
+        back->next = temp;
+        back = temp;
     }
     return head;
 }
@@ -61,11 +61,31 @@ vector<pair<int, int>> findPairs(Node* head, int k)
     return ans;
 }
 
+
+vector<pair<int, int>> findPairsOptimal(Node* head, int k)
+{
+    // Write your code here.
+    Node* left = head;
+    Node* right = head;
+    vector<pair<int, int>> ans;
+    while(right->next != NULL) right = right->next;
+    while(left->data < right->data){
+        if((left->data + right->data) == k) {
+            ans.push_back({left->data, right->data});
+            left = left->next;
+            right = right->back;
+            };
+        if((left->data + right->data) < k) left = left->next;
+        if((left->data + right->data) > k) right = right->back;
+    }
+    return ans;
+}
+
 int main(){
     vector<int> arr = {1,2,3,4,9};
     Node* head = convertArray2DLL(arr);
     print(head);
-    vector<pair<int, int>> pair = findPairs(head, 5);
+    vector<pair<int, int>> pair = findPairsOptimal(head, 5);
     cout << endl;
     for(auto &a: pair){
         cout << a.first << " " << a.second << endl;
