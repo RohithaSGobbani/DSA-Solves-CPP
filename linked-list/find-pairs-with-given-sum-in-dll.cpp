66f14cbb39c195148bde07557316754d4eeeb86f
RohithaSGobbani <rohitshreyas887@gmail.com>
2026-05-12 19:32:44 +0530

    feat: add find-pairs-with-given-sum-in-dll brute solution

---
+++ linked-list/find-pairs-with-given-sum-in-dll.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node* back;
+
+    public:
+    Node(int data1, Node* next1, Node* back1){
+        data = data1;
+        next = next1;
+        back = back1;
+    }
+
+    public:
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+        back = nullptr;
+    }
+};
+
+Node* convertArray2DLL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* prev = head;
+    int n = arr.size();
+    for(int i = 1; i < n; i++){
+        Node* temp = new Node(arr[i], nullptr, prev);
+        prev->next = temp;
+        prev = temp;
+    }
+    return head;
+}
+
+void print(Node* head){
+    Node* temp = head;
+    while(temp){
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;    
+}
+
+vector<pair<int, int>> findPairs(Node* head, int k)
+{
+    Node* t1 = head;
+    vector<pair<int, int>> ans;
+    while(t1 != NULL){
+        Node* t2 = t1->next;
+
+        while(t2 != NULL){
+            int sum = 0;
+            sum += t1->data + t2->data;
+            if(sum == k) ans.push_back({t1->data, t2->data});
+            t2 = t2->next;
+        }
+        t1 = t1->next;
+    }
+    return ans;
+}
+
+int main(){
+    vector<int> arr = {1,2,3,4,9};
+    Node* head = convertArray2DLL(arr);
+    print(head);
+    vector<pair<int, int>> pair = findPairs(head, 5);
+    cout << endl;
+    for(auto &a: pair){
+        cout << a.first << " " << a.second << endl;
+    }
+    return 0;
+}
