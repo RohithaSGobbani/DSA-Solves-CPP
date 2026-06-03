9685f43a7590fb2fde345dfb8c978e8ff733d531
RohithaSGobbani <rohitshreyas887@gmail.com>
2026-06-04 02:06:31 +0530

    feat: add remove-duplicates-from-sorted-list-II solution

---
+++ linked-list/remove-duplicates-from-sorted-list-II.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    public:
+    Node(int data1, Node* next1){
+        data = data1;
+        next = next1;
+    }
+
+    public:
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+
+Node* convertArray2LL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i = 1; i < arr.size(); i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = mover->next;
+    }
+    return head;
+}
+
+void print(Node* head){
+    Node* temp = head;
+    while(temp){
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;    
+}
+
+Node* deleteDuplicates(Node* head) {
+    if(head == NULL || head->next == NULL) return head;
+    Node* temp = head;
+    Node* dummy = new Node(-1);
+    dummy->next = head;
+    Node* res = dummy;
+    while(temp && temp->next){
+        if(temp->data == temp->next->data){
+            while(temp->next && temp->data == temp->next->data){
+                temp = temp->next;
+            }
+            res->next = temp->next;
+        }else{
+            res = res->next;
+        }
+        temp = temp->next;
+    }
+    return dummy->next;
+}
+
+/*
+Input: head = [1,1,2,3,3]
+Output: [2]
+*/
+
+int main(){
+    vector<int> arr = {1,1,2,3,3};
+    Node* head = convertArray2LL(arr);
+    print(head);
+    head = deleteDuplicates(head);
+    print(head);
+    return 0;
+}
