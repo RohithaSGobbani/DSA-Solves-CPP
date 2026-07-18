c80144d4556fbc78c9501fc490015daafb0fe31d
RohithaSGobbani <rohitshreyas887@gmail.com>
2026-07-18 17:00:59 +0530

    feat: add permutations optimal solution

--- recursion/permutations.cpp
+++ recursion/permutations.cpp
@@ -28,3 +28,23 @@ vector<vector<int>> permute(vector<int>& nums) {
 Input: nums = [1,2,3]
 Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
 */
+
+
+//Optimal
+void permute(int i, vector<int> &nums, vector<vector<int>> &ans){
+    if(i == nums.size()){
+        ans.push_back(nums);
+        return;
+    }
+    for(int j = i; j < nums.size(); j++){
+        swap(nums[i] , nums[j]);
+        permute(i+1, nums, ans);
+        swap(nums[i], nums[j]);
+    }
+}
+
+vector<vector<int>> permute(vector<int>& nums) {
+    vector<vector<int>> ans;
+    permute(0, nums, ans);
+    return ans;
+}
