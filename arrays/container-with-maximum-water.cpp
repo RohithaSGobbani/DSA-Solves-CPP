cea461a5c76cb4cc8da339bb759f189f35de957b
RohithaSGobbani <rohitshreyas887@gmail.com>
2026-07-23 22:51:42 +0530

    feat: add container-with-maximum-water solution

---
+++ arrays/container-with-maximum-water.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Brute
+int maxAreaBrute(vector<int>& height) {
+    int n = height.size();
+    int j = 0;
+    int mxwater = INT_MIN;
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){             
+            int width = j - i;
+            int h = (height[i] > height[j]) ? height[j] : height[i];
+            int water = width * h;
+            mxwater = max(mxwater, water);
+        }
+    }
+    return mxwater;
+}
+
+/*
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+*/
+
+int main(){
+    vector<int> arr = {1,8,6,2,5,4,8,3,7};
+    cout << maxAreaBrute(arr);
+    return 0;
+}
