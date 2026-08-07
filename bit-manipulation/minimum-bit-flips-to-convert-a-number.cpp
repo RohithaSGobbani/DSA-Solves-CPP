f381a358634015a2d33ac9b988fa9486b014317b
RohithaSGobbani <rohitshreyas887@gmail.com>
2026-08-07 19:07:04 +0530

    feat: add minimum-bit-flips-to-convert-a-number solution

---
+++ bit-manipulation/minimum-bit-flips-to-convert-a-number.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Brute
+string convert2Bin(int num){
+        string ans = "";
+        while(num > 0){
+            if(num % 2 == 1) ans += '1';
+            else ans += '0';
+            num = num / 2;
+        }
+        while(ans.size() != 32) ans += '0';
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+    int minBitFlips(int start, int goal) {
+        string st = convert2Bin(start);
+        string go = convert2Bin(goal);
+        int cnt = 0;
+        cout << st << " " << go;
+        int n1 = st.size();
+        int n2 = go.size();
+        while(n1 >= 0 && n2 >= 0){
+            if(st[n1] != go[n2]) cnt++;
+            n1--;
+            n2--;
+        }
+        return cnt;
+}
+
+int main(){
+	cout << minBitFlips(32, 14);
+	return 0;
+}
