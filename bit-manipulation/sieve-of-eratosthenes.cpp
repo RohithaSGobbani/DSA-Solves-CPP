228373ade83d611067df3557f0b69069d26c82f8
RohithaSGobbani <rohitshreyas887@gmail.com>
2026-08-12 22:52:11 +0530

    feat: add sieve-of-eratosthenes solution

---
+++ bit-manipulation/sieve-of-eratosthenes.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+int countPrimes(int n) {
+        int prime[n+1];
+        for(int i = 2; i < n; i++){
+            prime[i] = 1;
+        }
+        for(int i = 2; i < sqrt(n); i++){
+            if(prime[i] == 1){
+                for(int j = i*i; j <=n; j+=i){
+                    prime[j] = 0;
+                }
+            }
+        }
+        int cnt = 0;
+        for(int i = 2; i < n; i++){
+            if(prime[i] == 1) cnt++;
+        }
+        return cnt;
+}
+
+int main(){
+	cout << countPrimes(15);
+	return 0;
+}
