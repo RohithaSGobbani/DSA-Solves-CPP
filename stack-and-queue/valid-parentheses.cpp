94773ff0eec629fd5f476f6865dd3cfa3388e876
RohithaSGobbani <rohitshreyas887@gmail.com>
2026-09-12 00:16:24 +0530

    feat: add valid-parentheses solution

---
+++ stack-and-queue/valid-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n = s.size();
+        stack<char> st;
+        for(int i = 0;i < n; i++){
+            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+                st.push(s[i]);
+            }
+            else{
+                if(st.empty()){
+                    return false;
+                }else {
+                    char ch = st.top();
+                    if((s[i] == ')' && ch == '(') || (s[i] == '}' && ch == '{') || (s[i] == ']' && ch == '[')){
+                        st.pop();
+                    }else return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+};
